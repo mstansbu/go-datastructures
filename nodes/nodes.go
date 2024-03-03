29fff49264c05203b5d4e78c5423deec5cc1fa3a
Mattie Stansbury <mstansbu@gmail.com>
2024-03-03 08:38:04 -0800

    added tuple and double linked list node

--- nodes/nodes.go
+++ nodes/nodes.go
@@ -8,3 +8,22 @@ type Node[T comparable] struct {
 func (this *Node[T]) NewNode(value T) Node[T] {
 	return Node[T]{Val: value, Next: nil}
 }
+
+type Tuple[T any, N any] struct {
+	First  T
+	Second N
+}
+
+func (this *Tuple[T, N]) NewTuple(t T, n N) Tuple[T, N] {
+	return Tuple[T, N]{t, n}
+}
+
+type DLNode[T comparable] struct {
+	Val  T
+	Next *DLNode[T]
+	Prev *DLNode[T]
+}
+
+func (this *DLNode[T]) NewDLNode(value T) DLNode[T] {
+	return DLNode[T]{Val: value, Next: nil, Prev: nil}
+}
